21837da436b8aca16d37135bb833f62f69694cf8
patogrins <patogrins@hotmail.com>
2026-06-25 12:31:13 -0300

    Increase logo image width in TabInicio component

--- src/TabInicio.jsx
+++ src/TabInicio.jsx
@@ -184,7 +184,7 @@ export default function TabInicio({ usuario, esAdmin, esPublico, t, onLogin, res
           transition: "none",
           pointerEvents: "none",
         }}>
-          <img src="/logohead.jpeg" alt="GRINS" style={{ width: 20000, objectFit: "contain", opacity: 0.95 }} />
+          <img src="/logohead.jpeg" alt="GRINS" style={{ width: 100000, objectFit: "contain", opacity: 0.95 }} />
         </div>
 
         {/* PERFIL — se achica y sube al scroll */}
